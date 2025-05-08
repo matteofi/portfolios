649a2cdd38bc2a84788da457f07e1e6820d83b04
matteofilippini <88026480+matteofi@users.noreply.github.com>
2025-05-08 09:31:04 +0200

    Add files via upload

--- portfolios/settings.py
+++ portfolios/settings.py
@@ -124,10 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolios2/static'),
+    BASE_DIR / "static",  # se lo metti dentro una cartella "static"
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Aggiungi queste impostazioni di sicurezza per Render
 CSRF_COOKIE_SECURE = True
